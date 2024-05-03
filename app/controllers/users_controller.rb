252c10be8c97cbbf980b66f136aa2840e5247519
vinnie <githinjigicheru146@gmail.com>
2024-05-03 16:04:40 +0300

    Associate user with items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
+
+    private
+
+    def render_record_not_found
+        render json: {error: "Record not found."}, status: :unprocessable_entity
+    end
+
 end
